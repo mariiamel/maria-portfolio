39f23c2ac1e62ce8f1393b018e7b35a8722c16b4
mariiamel <mariiamelnikova96@gmail.com>
2021-05-25 16:08:44 +0500

    home component done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Typical from 'react-typical'
 
 const Home = () => {
     return (
         <div className="header-wraper">
             <div className="main-info">
-                <h1>Software Engineer</h1>
+                <h1><span className='tags'>{'<'}</span>Maria Melnikova<span className='tags'>{'/>'}</span></h1>
+                <Typical
+                    className='typical-style'
+                    steps={['I am a Software Engineer 💻', 500, 'Full Stack Web Developer 🖇', 500, 'Front End Web Developer ✨', 500]}
+                    loop={Infinity}
+                    wrapper="p"
+                />
             </div>
         </div>
     )
