4a8e6b9ba85660a7b3de746adaa0cc23347e6e1f
mariiamel <mariiamelnikova96@gmail.com>
2021-05-26 16:02:19 +0500

    contact me component done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,14 +5,14 @@ import pdf from '../assets/maria_melnikova_resume.pdf'
 
 export default function Contact () {
     return(
-        <div>
+        <div className='contact-component'>
             <h1 className="contact-me">Contact Me</h1>
                 <div className="contact">
-                    <div><a href="https://www.linkedin.com/in/mariia-mel/" target="_blank" rel='noreferrer noopener'><FontAwesomeIcon className="icon-social" icon={faLinkedin} /></a></div>
-                    <div><a href="https://github.com/mariiamel" target="_blank" rel='noreferrer noopener'><FontAwesomeIcon className="icon-social" icon={faGithub} /></a></div>
-                    <div><a href="mailto: mariiamelnikova96@gmail.com"><FontAwesomeIcon className="icon-social" icon={faEnvelope} /></a></div>
-                    <div><a href={pdf} target="_blank" className="cv icon-social" rel='noreferrer noopener'>CV</a></div>
+                    <div><a href="https://www.linkedin.com/in/mariia-mel/" target="_blank" rel='noreferrer noopener'><FontAwesomeIcon className="icon-social-contact" icon={faLinkedin} /></a></div>
+                    <div><a href="https://github.com/mariiamel" target="_blank" rel='noreferrer noopener'><FontAwesomeIcon className="icon-social-contact" icon={faGithub} /></a></div>
+                    <div><a href="mailto: mariiamelnikova96@gmail.com"><FontAwesomeIcon className="icon-social-contact" icon={faEnvelope} /></a></div>
                 </div>
+                    <p><a href={pdf} target="_blank" className="cv icon-social-contact" rel='noreferrer noopener'>Resume</a></p>
         </div>
     )
 }
