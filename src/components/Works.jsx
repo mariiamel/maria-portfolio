6dc26af77a4d7a5d62dc2e7f27ef1dae03ee282d
mariiamel <mariiamelnikova96@gmail.com>
2021-06-02 13:44:04 +0500

    more styling

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,22 +15,18 @@ export default function Works () {
                     <span className='project'>
                         <img src={tripTracker} alt='trip tracker project'  className='trip'/>
 
-                        <p>MERN Full-Stack Web Application allowing users to stay organized while traveling.
-                        Implemented MongoDB database with Mongoose ODM and Express.js on the Back end and React on the Front end
-                        Dynamically integrated asynchronous Axios API calls with minimization efforts to reduce latency and increase app’s speed and overall smoothness
-                        Built JSON Web Tokens for users authentication
-                        </p>
+                        <p><b>MERN Full-Stack Web Application</b> allowing users to stay organized while traveling.</p>
+                        <p>Implemented MongoDB database with Mongoose ODM and Express.js on the Back end and React on the Front end</p>
+                        <p>Dynamically integrated asynchronous Axios API calls with minimization efforts to reduce latency and increase app’s speed and overall smoothness</p>
+                        <p>Built JSON Web Tokens for users authentication</p>
+                        
                         <p><b>Tech:</b> React, Javascript, Node.js, Express.js, Mongoose, MongoDB, Bcrypt, Axios, JWT, HTML and CSS</p>
                         <p><b>Login:</b> email - user@new.com, password - usernew</p>
 
-                        <button className='btn-left'>
-                            <a href='https://triptracker.netlify.app/' target="_blank" rel='noreferrer noopener'>Demo</a>
-                        </button>
-                        <button className='btn-left'>
-                            <a href="https://github.com/mariiamel/project3_client" target="_blank" rel='noreferrer noopener'>
+                            <a href='https://triptracker.netlify.app/' target="_blank" rel='noreferrer noopener' className='btn-left'>Demo</a>
+                            <a href="https://github.com/mariiamel/project3_client" target="_blank" rel='noreferrer noopener' className='btn-left'>
                                 <FontAwesomeIcon className="icon-social" icon={faGithub} />
                             </a>
-                        </button>
                     </span>
                 </div>
 
@@ -38,20 +34,17 @@ export default function Works () {
                     <h1>CoffeeMe</h1>
                     <span className='project'>
                         <img src={coffeeme} alt='coffee me project' className='coffeee'/>
-                        <p>Full- Stack Web Application allowing users to search for the coffeeshops and add them to favorite list.
-                        Coded a User login system, storing encrypted information in cookies and SQL, to allow a specific user experience while maintaining user privacy
-                        Exported data from APIs to SQL database utilizing Javascript and EJS to render searched coffeeshops where readability and organization are maximized Implemented the entire site through sequelize, following full RESTful routing, giving the user access to full CRUD functionality on the site</p>
+                        <p><b>Full- Stack Web Application</b> allowing users to search for the coffeeshops and add them to favorite list.</p>
+                        <p>Coded a User login system, storing encrypted information in cookies and SQL, to allow a specific user experience while maintaining user privacy</p>
+                        <p>Exported data from APIs to SQL database utilizing Javascript and EJS to render searched coffeeshops where readability and organization are maximized Implemented the entire site through sequelize, following full RESTful routing, giving the user access to full CRUD functionality on the site</p>
                         <p><b>Tech:</b> Javascript, EJS, Sequelize, Postgres, Express.js, Node.js, API, axios</p>
                         <p><b>Login:</b> username: test, email: test@gmail.com, password:test</p>
                         
-                        <button className='btn-right'>
-                            <a href='https://second-project-coffeeme.herokuapp.com/' target="_blank" rel='noreferrer noopener'>Demo</a>
-                        </button>
-                        <button className='btn-right'>
-                            <a href="https://github.com/mariiamel/Project_2" target="_blank" rel='noreferrer noopener'>
+                            <a href='https://second-project-coffeeme.herokuapp.com/' target="_blank" rel='noreferrer noopener' className='btn-right'>Demo</a>
+
+                            <a href="https://github.com/mariiamel/Project_2" target="_blank" rel='noreferrer noopener' className='btn-right'>
                                 <FontAwesomeIcon className="icon-social" icon={faGithub} />
                             </a>
-                        </button>
                     </span>
                 </div>
 
@@ -60,19 +53,16 @@ export default function Works () {
                     <span className='project'>
                         <img src={guess} alt='guess where game' className='guess'/>
 
-                        <p>Front-End Web Game.
-                        Challenge yourself to see if you can guess where the item is hidden
-                        Developed an interactive gaming application with functional Javascript logic that allows a user to guess on its attributes against a computer opponent Built unique Javascript functionality to keep track of user’s level, attempts, items to guess that is based on real value</p>
+                        <p><b>Front-End Web Game.</b></p>
+                        <p>Challenge yourself to see if you can guess where the item is hidden</p>
+                        <p>Developed an interactive gaming application with functional Javascript logic that allows a user to guess on its attributes against a computer opponent</p>
+                        <p>Built unique Javascript functionality to keep track of user’s level, attempts, items to guess that is based on real value</p>
                         <p><b>Tech:</b> Javascript, HTML, CSS(Flexbox) and DOM manipulation</p>
 
-                        <button className='btn-left'>
-                            <a href='https://mariiamel.github.io/' target="_blank" rel='noreferrer noopener'>Demo</a>
-                        </button>
-                        <button className='btn-left'>
-                            <a href="https://github.com/mariiamel/Guess-where-Game" target="_blank" rel='noreferrer noopener'>
+                            <a href='https://mariiamel.github.io/' target="_blank" rel='noreferrer noopener' className='btn-left'>Demo</a>
+                            <a href="https://github.com/mariiamel/Guess-where-Game" target="_blank" rel='noreferrer noopener' className='btn-left'>
                                 <FontAwesomeIcon className="icon-social" icon={faGithub} />
                             </a>
-                        </button>
                     </span>
                 </div>
             </div>
